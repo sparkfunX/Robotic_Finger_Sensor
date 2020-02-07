90844e43cba5000f5b94ab2762deed1c0213c133
pmakin-png <priyanka.makin@sparkfun.com>
2020-02-07 15:35:07 -0700

    Added test script

---
+++ RoboticFinger_Test/RoboticFinger_Test.ino
@@ -0,0 +1,64 @@
+/*
+  Robotic Finger Sensor - Basic examples
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: June 12th, 2018
+  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware License).
+
+  Outputs the IR Proximity Value from the VCNL4040 sensor and the pressure reading and
+  temp from the LPS25HB pressure sensor.
+
+  Hardware Connections:
+  If needed, attach a Qwiic Shield to your Arduino/Photon/ESP32 or other
+  Plug the device into an available Qwiic port
+  Open the serial monitor at 9600 baud to see the output
+*/
+
+#include <Wire.h>
+
+#include "SparkFun_VCNL4040_Arduino_Library.h" //Library: http://librarymanager/All#SparkFun_VCNL4040
+VCNL4040 proximitySensor;
+
+#include "SparkFun_LPS25HB_Arduino_Library.h"  //Library: http://librarymanager/All#SparkFun_LPS25HB
+LPS25HB pressureSensor;
+
+const int LED = 13;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Robotic Finger Sensor v2 Example");
+
+  Wire.begin();
+  Wire.setClock(400000); //Increase I2C bus speed to 400kHz
+
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  pressureSensor.begin();
+  if (proximitySensor.begin() == true && pressureSensor.isConnected() == true)
+  { 
+    digitalWrite(LED, HIGH);
+    
+    unsigned int proxValue = proximitySensor.getProximity();
+
+    float pressure = pressureSensor.getPressure_hPa();
+    float temp = pressureSensor.getTemperature_degC();
+
+    Serial.print("Proximity Value: ");
+    Serial.print(proxValue);
+
+    Serial.print(" Pressure in hPa: ");
+    Serial.print(pressure);
+    Serial.print(", Temperature (degC): ");
+    Serial.println(temp);
+  }
+  else {
+    Serial.println("Nothing detected.");
+    digitalWrite(LED, LOW);
+  }
+
+  delay(10);
+}
